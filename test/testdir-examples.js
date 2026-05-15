9a06ff1a1aca49328e367c0aba4434a51e2f7772
Michael Smith <owlstronaut@github.com>
2026-05-15 09:44:34 -0700

    chore: run lintfix

--- test/testdir-examples.js
+++ test/testdir-examples.js
@@ -19,7 +19,7 @@ test('creates simple package.json structure', async (t) => {
   const pkgPath = path.join(dir, 'package.json')
   const pkgContent = await fs.readFile(pkgPath, 'utf8')
   const pkg = JSON.parse(pkgContent)
-  
+
   assert.strictEqual(pkg.name, 'test-pkg')
   assert.strictEqual(pkg.version, '1.0.0')
   assert.deepStrictEqual(pkg.dependencies, { foo: '^1.0.0' })
@@ -59,12 +59,12 @@ test('creates workspace structure with multiple packages', async (t) => {
   // Verify root package.json
   const rootPkg = JSON.parse(await fs.readFile(path.join(dir, 'package.json'), 'utf8'))
   assert.deepStrictEqual(rootPkg.workspaces, ['packages/*'])
-  
+
   // Verify workspace packages
   const pkgA = JSON.parse(await fs.readFile(path.join(dir, 'packages/a/package.json'), 'utf8'))
   assert.strictEqual(pkgA.name, 'a')
   assert.strictEqual(pkgA.version, '1.2.3')
-  
+
   const pkgB = JSON.parse(await fs.readFile(path.join(dir, 'packages/b/package.json'), 'utf8'))
   assert.strictEqual(pkgB.name, 'b')
   assert.strictEqual(pkgB.version, '1.2.3')
@@ -90,10 +90,10 @@ test('creates mixed content types', async (t) => {
   // Verify files exist with correct content
   const readme = await fs.readFile(path.join(dir, 'README.md'), 'utf8')
   assert.strictEqual(readme, '# Test Package\n\nThis is a test.')
-  
+
   const indexJs = await fs.readFile(path.join(dir, 'lib/index.js'), 'utf8')
   assert.strictEqual(indexJs, 'module.exports = "hello"')
-  
+
   const guideExists = await fs.access(path.join(dir, 'docs/guide.md'))
     .then(() => true)
     .catch(() => false)
@@ -112,7 +112,7 @@ test('creates symlinks', async (t) => {
   // Verify symlink exists and points to target
   const stats = await fs.lstat(path.join(dir, 'link-to-target'))
   assert.strictEqual(stats.isSymbolicLink(), true)
-  
+
   const linkTarget = await fs.readlink(path.join(dir, 'link-to-target'))
   assert.strictEqual(linkTarget, 'target')
 })
@@ -122,18 +122,18 @@ test('multiple calls reuse same directory', async (t) => {
   const dir1 = await testdir(t, {
     'file1.txt': 'content 1',
   })
-  
+
   const dir2 = await testdir(t, {
     'file2.txt': 'content 2',
   })
-  
+
   // Should be the same directory
   assert.strictEqual(dir1, dir2)
-  
+
   // Both files should exist
   const file1Content = await fs.readFile(path.join(dir1, 'file1.txt'), 'utf8')
   const file2Content = await fs.readFile(path.join(dir2, 'file2.txt'), 'utf8')
-  
+
   assert.strictEqual(file1Content, 'content 1')
   assert.strictEqual(file2Content, 'content 2')
 })
@@ -180,13 +180,13 @@ test('creates complex nested node_modules structure', async (t) => {
   // Verify the structure
   const rootPkg = JSON.parse(await fs.readFile(path.join(dir, 'package.json'), 'utf8'))
   assert.strictEqual(rootPkg.name, 'complex-app')
-  
+
   const fooPkg = JSON.parse(await fs.readFile(path.join(dir, 'node_modules/foo/package.json'), 'utf8'))
   assert.strictEqual(fooPkg.name, 'foo')
-  
+
   const barIndex = await fs.readFile(path.join(dir, 'node_modules/bar/index.js'), 'utf8')
   assert.strictEqual(barIndex, 'module.exports = "bar"')
-  
+
   const fooDepPkg = JSON.parse(await fs.readFile(path.join(dir, 'node_modules/foo-dep/package.json'), 'utf8'))
   assert.strictEqual(fooDepPkg.name, 'foo-dep')
 })
